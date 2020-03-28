3519f17e31b7be85bbc165fdb2e753b98d0d105a
Murali Jayaraman <subha.engr@gmail.com>
2020-03-28 15:57:23 -0700

    (1) formatted code (2) moved comment (3) added a single line

--- BasicMaven/src/main/java/com/bayamp/pleasanton2020/interview/questions/RandomProgram.java
+++ BasicMaven/src/main/java/com/bayamp/pleasanton2020/interview/questions/RandomProgram.java
@@ -1,17 +1,16 @@
 package com.bayamp.pleasanton2020.interview.questions;
 
 import com.bayamp.pleasanton2020.utils.RandomUtil;
-import com.sun.javafx.binding.StringFormatter;
 
-import java.util.*;
-
+/**
+ * author name :Subha
+ */
 public class RandomProgram {
-    /** author name :Subha */
-
+    
     public static void main(String[] args) {
 
         for (int i = 0; i < 10; i++) {
-            String phoneNumber =RandomUtil.generateRandomPhoneNumber();
+            String phoneNumber = RandomUtil.generateRandomPhoneNumber();
             System.out.println(phoneNumber);
         }
     }
